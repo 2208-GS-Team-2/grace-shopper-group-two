ec39a3b08894f4971f2280d38b54805b5f1dbc6d
Anthony Hang <hang.anthony@gmail.com>
2022-12-06 19:33:50 -0500

    fixed scripts to allow separate startdev no seed and startdev with seed

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const app = require('./app');
+const seed = require('./db/seed');
 
 const init = async () => {
     try {
+        if(process.env.seed=== "false") await seed();
         const port = process.env.PORT || 3000;
         app.listen(port, () => console.log(`🚀listening on port: ${port} 🛒🔗 http://localhost:${port} 🛒`));
     }
