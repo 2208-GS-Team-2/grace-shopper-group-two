6269791b5b03d41365d06b518e160d4c6e74d8d4
Lena Al <lena.al.developer@gmail.com>
2022-12-07 16:44:31 -0800

    still working on single product page

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -1,35 +1,40 @@
+import { Card, CardContent, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, Link } from 'react-router-dom';
-import {
-  setSingleProduct,
-  setLoadingProduct,
-} from '../store/singleProductSlice';
+import { useParams } from 'react-router-dom';
 
-const SingleCampusPage = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const { singleProduct, setLoadingProduct } = useSelector(
-    (state) => state.singleProduct
-  );
+const SingleProduct = () => {
+  // const { id } = useParams();
+  // const dispatch = useDispatch();
+  const { singleProduct } = useSelector((state) => state.product);
+  // if (!Object.keys(singleProduct).length) {
+  //   return (
+  //     <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+  //       <CircularProgress />
+  //     </div>
+  //   );
+  // }
 
-  const fetchSingleProduct = async () => {
-    dispatch(setLoadingProduct(true));
-    const response = await axios.get(
-      `http://localhost:3000/api/products/${id}`
-    );
-    dispatch(setSingleProduct(response.data));
-    dispatch(setLoadingProduct(false));
-  };
-
-  useEffect(() => {
-    fetchSingleProduct();
-  }, []);
-
-  if (loadingProduct) return <p>Loading...</p>;
-
-  return <div></div>;
+  // const renderProductData = (
+  //   <div>
+  //     <div
+  //       style={{ display: 'flex', justifyContent: 'center', margin: '150px' }}
+  //     >
+  //       <Card sx={{ maxWidth: 800 }} style={{ display: 'flex' }}>
+  //         <CardContent style={{ padding: '5px;' }}>
+  //           <Typography variant="h3">{selectedCampus.name}</Typography>
+  //           <Typography variant="body2" color="text.secondary">
+  //             location: {selectedCampus.address}
+  //           </Typography>
+  //           <Typography variant="h6">{selectedCampus.description}</Typography>
+  //         </CardContent>
+  //       </Card>
+  //       <div></div>
+  //     </div>
+  //   </div>
+  // );
+  return <div>"hello"</div>;
 };
 
-export default SingleProductPage;
+export default SingleProduct;
