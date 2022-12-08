59249ca3455f3c589cc3d939d55381885883b1aa
Stephen Werbeck <SWerbeck@yahoo.com>
2022-12-08 17:27:26 -0500

    created create user component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { resetUser } from '../store/userSlice';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { resetUser } from "../store/userSlice";
+import CreateUserPage from "./CreateUser";
 
 const Home = () => {
-    const { user } = useSelector(state => state.user);
-    const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-    const logout = () => {
-        window.localStorage.removeItem('token');
-        dispatch(resetUser());
-    };
+  const logout = () => {
+    window.localStorage.removeItem("token");
+    dispatch(resetUser());
+  };
 
-    return (
-        <div>
-            <h1>Home</h1>
-            <div>
-                <p>Welcome {user.username}!!</p>
-                <button onClick={logout}>Logout</button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <p>Welcome {user.username}!!</p>
+
+        <button onClick={logout}>Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
