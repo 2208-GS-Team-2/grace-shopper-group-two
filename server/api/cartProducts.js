9b1b68521e650702d90d93829388e7645ef3be24
Stephen Werbeck <SWerbeck@yahoo.com>
2022-12-18 13:59:33 -0500

    got product qty update api route working

--- server/api/cartProducts.js
+++ server/api/cartProducts.js
@@ -2,6 +2,16 @@ const express = require("express");
 const { CartProduct } = require("../db");
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  try {
+    const cartProduct = await CartProduct.findAll();
+
+    res.send(cartProduct);
+  } catch (err) {
+    return res.status(404).send(err.message);
+  }
+});
+
 router.put("/", async (req, res, next) => {
   try {
     const productId = req.body.productId;
@@ -13,6 +23,41 @@ router.put("/", async (req, res, next) => {
 
     await deleteProductFromCart.destroy();
     res.send(202);
+  } catch (err) {
+    return res.status(404).send(err.message);
+  }
+});
+
+router.put("/update", async (req, res, next) => {
+  try {
+    const productId = req.body.productId;
+    const cartId = req.body.cartId;
+    const productQuantity = req.body.productQuantity;
+
+    const cartProduct = await CartProduct.findOne({
+      where: { productId: productId, cartId: cartId },
+    });
+
+    if (productQuantity === "0") {
+      await cartProduct.destroy();
+      return res.send("product removed from cart");
+    }
+
+    await cartProduct.update({
+      productQuantity: productQuantity,
+    });
+    res.send("product quantity updated");
+  } catch (err) {
+    return res.status(404).send(err.message);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = req.params;
+    const cartProduct = await CartProduct.findByPk(id);
+
+    res.send(cartProduct);
   } catch (err) {
     return res.status(404).send(err.message);
   }
