cfd78897860f644b96e4de4ab5f03c5e1b1daf97
Antongeny <hang.anthony@gmail.com>
2023-02-23 17:35:08 -0500

    removed await from seed()

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const seed = require("./db/seed");
 
 const init = async () => {
 	try {
-		if (process.env.SEED === "true") await seed();
+		if (process.env.SEED === "true") seed();
 		const port = process.env.PORT || 3000;
 		app.listen(port, () => console.log(`🚀listening on port: ${port} 🛒🔗 http://localhost:${port} 🛒`));
 	} catch (ex) {
